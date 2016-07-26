a921990ddd44a7ba06708b25e3499281e184e3bd
kouryuu <antarticfreeze@gmail.com>
2016-07-25 19:48:00 -0500

    Added next function to props

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,7 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import SmallText from './question-types/small-text-input.js'
 import SmallMultipleOptions from './question-types/small-multiple-options.js'
+let nextFunction = (function(event){
+console.log(event);
+})
 
 let myoptions = [{id:1,description:"Red",selected:false},{id:2,description:"Orange",selected:false}];
-ReactDOM.render(<div>< SmallMultipleOptions question="Choose your favorite colors" options={myoptions}/>
-<SmallText question="How are you?" /></div>,document.getElementById('content'));
+ReactDOM.render(<div>
+  <SmallMultipleOptions question="Choose your favorite colors" options={myoptions}/>
+<SmallText question="How are you?" skippable={true} nextFunc={nextFunction.bind(this)}/>
+</div>,document.getElementById('content'));
