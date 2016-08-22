1033d91fb2782b92c6b2c68cf3612952261a34fb
kouryuu <antarticfreeze@gmail.com>
2016-08-22 18:06:23 -0500

    Deleted hardcoded props to be defined in the questions.json

--- app/scripts/questionnaire.js
+++ app/scripts/questionnaire.js
@@ -1,12 +1,10 @@
 import React from 'react'
-import SmallText from './question-types/small-text-input.js'
-import SmallMultipleOptions from './question-types/small-multiple-options.js'
-import SmallUniqueOptions from './question-types/small-unique-options.js'
-import NextButton from './ui-lib/next-button.js'
 import {store} from './store.js'
 import Tooltip from './ui-lib/tooltip.js'
+import generate_questions from './generate_questions.js'
+import NextButton from './ui-lib/next-button.js'
+import ClosingMessage from './ui-lib/closing-message.js'
 export default class Questionnaire extends React.Component{
-
   constructor(){
     super();
     this.state = {
@@ -14,41 +12,31 @@ export default class Questionnaire extends React.Component{
       tipAlert:false
     }
   }
-
   _nextFunction(skip){
     //event.preventDefault();
-
+    console.log(store()[this.state.currentQuestion]);
     if(skip ||
        (store()[this.state.currentQuestion] !== undefined && store()[this.state.currentQuestion] !== "")){
     this.setState({tipAlert:false});
     this.setState({currentQuestion:this.state.currentQuestion+1});
-  }else{
+    }else{
     this.setState({tipAlert:true});
-  }
+    }
   }
   render(){
-    let myoptions = [{id:1,description:"Pizza",selected:false},{id:2,description:"Hamburger",selected:false}];
-    let myoptions2 = [{id:1,description:"Red",selected:false},{id:2,description:"Orange",selected:false}];
-    return (<div>
-      {(()=>{ switch(this.state.currentQuestion)
-       {
-      case 2:
-      return (<div className="well"><SmallUniqueOptions  questionNumber={2}  question="Choose your favorite food" options={myoptions} /><NextButton skippable={false} nextFunc={this._nextFunction.bind(this,false)}/></div>);
-      case 1:
-        return (<div className="well"><SmallMultipleOptions questionNumber={1} question="Choose your favorite color"  options={myoptions2} /><NextButton skippable={false} nextFunc={this._nextFunction.bind(this,false)}/></div>);
-      case 0:
-        return (<div className="well"><SmallText questionNumber={0} question="How are you?" /><NextButton skippable={false} nextFunc={this._nextFunction.bind(this,false)}/></div>);
-
-      }
+    let questions = generate_questions(this.props.questions,this._nextFunction,this);
+    return (
+    <div>
+      {(()=>{
+        if(this.state.currentQuestion === 0)
+          return (<div className="well"><h2 className="text-center">{this.props.questions.configs.welcome}</h2><p className="text-center">{this.props.questions.configs.tip}</p><NextButton skippable={false} nextFunc={this._nextFunction.bind(this,true)}/></div>);
+        if(this.state.currentQuestion > 0 && this.state.currentQuestion <= questions.length)
+          return (questions[this.state.currentQuestion-1]);
+        return(<ClosingMessage message={this.props.questions.configs.closingmessage} tip={this.props.questions.closingtip} />)
     })()}
-    {(this.state.tipAlert)?<Tooltip message="You must answer to continue."></Tooltip>:null}
+    {(this.state.tipAlert)?<Tooltip message={this.props.questions.configs.tooltip}></Tooltip>:null}
 
   </div>);
-    return(
-      <div>
-
-      </div>
-    );
   }
 
 }
