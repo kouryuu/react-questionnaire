f838572ec2ac1c713261c30eb42679ef6cdce2b5
kouryuu <antarticfreeze@gmail.com>
2016-08-22 18:16:59 -0500

    Rewrote the main code to use JQuery  getJSON like a promise object.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,8 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Questionnaire from './questionnaire.js'
 
+let Errormsg = "Whoops! it looks like there is something wrong with the questions.";
 
-
+$.getJSON('/configs/questions.json')
+.done(function(data){
 ReactDOM.render(<div>
-            <Questionnaire />
+            <Questionnaire questions={data}/>
             </div>,document.getElementById('content'));
+})
+.fail(function(jqXHR, textStatus, errorThrown) {
+  ReactDOM.render(<div className="well">
+              {Errormsg}
+              </div>,document.getElementById('content'));
+  console.log('failed to get questions.json ' + textStatus);
+});
