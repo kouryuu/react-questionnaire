a31dfee385539c4f988c012db8844114373b2c8d
rreyes <antarticfreeze@gmail.com>
2016-09-23 19:09:27 -0500

    New results assets.

---
+++ results/scripts/app.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import QuestionColumn from './ui-lib/question-column.js';
+
+ReactDOM.render(<QuestionColumn test="TEST" />,document.getElementById('results'));
---
+++ results/scripts/ui-lib/question-column.js
@@ -0,0 +1,10 @@
+import React from 'react'
+export default class QuestionColumn extends React.Component{
+  render(){
+    return(
+      <div>
+      {this.props.test}
+      </div>
+    );
+  }
+}
